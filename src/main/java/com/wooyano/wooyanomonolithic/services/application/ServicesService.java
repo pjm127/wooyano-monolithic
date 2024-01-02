97a64d2872e58753fc5b46ff6890731af656590f
pjm127 <mikeywc40@gmail.com>
2024-01-02 22:04:40 +0900

    feat: ServicesService @Transactional 추가

--- src/main/java/com/wooyano/wooyanomonolithic/services/application/ServicesService.java
+++ src/main/java/com/wooyano/wooyanomonolithic/services/application/ServicesService.java
@@ -9,7 +9,9 @@ import com.wooyano.wooyanomonolithic.services.infrastructure.ServicesRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -19,6 +21,7 @@ public class ServicesService {
 
     private final ServiceTimeRepository serviceTimeRepository;
 
+    @Transactional
     public ServicesCreateResponse createService(ServicesCreateRequest request) {
         Services services = request.toEntity();
         Services save = servicesRepository.save(services);
