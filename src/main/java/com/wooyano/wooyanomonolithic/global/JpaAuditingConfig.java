287341f541502d5d268efceea3de0df176017820
pjm127 <mikeywc40@gmail.com>
2024-01-05 16:48:04 +0900

    refactor: @EnableJpaAuditing 분리

---
+++ src/main/java/com/wooyano/wooyanomonolithic/global/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.wooyano.wooyanomonolithic.global;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
