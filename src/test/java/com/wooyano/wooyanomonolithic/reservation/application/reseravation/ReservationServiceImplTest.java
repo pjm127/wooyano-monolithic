6a91109759ca9e8414ed5efa5b7c64d3106bb620
pjm127 <mikeywc40@gmail.com>
2024-01-11 15:33:33 +0900

    test: checkWorkerAvailability 메서드 해피케이스, 예외케이스 테스트 코드 구현

---
+++ src/test/java/com/wooyano/wooyanomonolithic/reservation/application/reseravation/ReservationServiceImplTest.java
@@ -0,0 +1,100 @@
+package com.wooyano.wooyanomonolithic.reservation.application.reseravation;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.wooyano.wooyanomonolithic.global.exception.CustomException;
+import com.wooyano.wooyanomonolithic.services.domain.ServiceTime;
+import com.wooyano.wooyanomonolithic.services.domain.Services;
+import com.wooyano.wooyanomonolithic.services.infrastructure.ServicesRepository;
+import com.wooyano.wooyanomonolithic.worker.domain.Worker;
+import com.wooyano.wooyanomonolithic.worker.domain.WorkerTime;
+import com.wooyano.wooyanomonolithic.worker.infrastructure.WorkerRepository;
+import com.wooyano.wooyanomonolithic.worker.infrastructure.WorkerTimeRepository;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Optional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceImplTest {
+
+    @Autowired
+    private ReservationServiceImpl reservationService;
+    @Autowired
+    private ServicesRepository servicesRepository;
+    @Autowired
+    private WorkerRepository workerRepository;
+    @Autowired
+    private WorkerTimeRepository workerTimeRepository;
+
+    @AfterEach
+    void tearDown() {
+        workerTimeRepository.deleteAllInBatch();
+        workerRepository.deleteAllInBatch();
+        servicesRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("해당 작업자의 예약 가능 여부를 확인한다")
+    @Test
+    public void checkWorkerAvailabilityWhenAvailable() {
+        // given
+        Worker worker = getWorker();
+        LocalDate reservationDate = LocalDate.now();
+        LocalTime serviceStart = LocalTime.of(10, 0, 0);
+
+        // when, then
+        assertDoesNotThrow(() ->
+                reservationService.checkWorkerAvailability(worker, reservationDate, serviceStart)
+        );
+    }
+
+
+    @DisplayName("해당 작업자의 예약 가능 여부를 확인하는데 이미 해당 작업자가 예약된 시간이 있으면 예외를 던진다")
+    @Test
+    public void checkWorkerAvailabilityWhenNotAvailable() {
+        // given
+        Worker worker = getWorker();
+        LocalDate reservationDate = LocalDate.now();
+        LocalTime serviceStart = LocalTime.of(9, 0,0);
+
+        // when, then
+        assertThrows(CustomException.class, () ->
+                reservationService.checkWorkerAvailability(worker, reservationDate, serviceStart)
+        );
+
+    }
+
+    private Worker getWorker() {
+        LocalTime openTime = LocalTime.of(9, 0, 0);
+        LocalTime closeTime = LocalTime.of(18, 0, 0);
+        ServiceTime servicesTime = ServiceTime.builder()
+                .openTime(openTime)
+                .closeTime(closeTime)
+                .build();
+        Services service = Services.builder()
+                .name("서비스1")
+                .description("서비스1 설명")
+                .serviceTime(servicesTime)
+                .build();
+        Services saveService = servicesRepository.save(service);
+        Worker worker = Worker.create("작업자1", "작업자1 폰", "작업자1 설명", saveService);
+        Worker saveWorker = workerRepository.save(worker);
+
+        WorkerTime workerTime = WorkerTime.createWorkerTime(LocalTime.of(9, 0, 0), saveWorker,
+                LocalDate.now());
+        workerTimeRepository.save(workerTime);
+        return worker;
+    }
+}
---
+++ src/test/java/com/wooyano/wooyanomonolithic/reservation/application/reseravation/TossRequestPaymentAcceptTest.java
@@ -0,0 +1,7 @@
+package com.wooyano.wooyanomonolithic.reservation.application.reseravation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TossRequestPaymentAcceptTest {
+
+}
