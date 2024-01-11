32d95a4b55a525a40b1d5d1b0bc49bbef2657736
pjm127 <mikeywc40@gmail.com>
2024-01-11 11:20:15 +0900

    feat: 웹훅 저장을 위한 ReservationHistory 엔티티 구현

--- src/main/java/com/wooyano/wooyanomonolithic/webhook/domain/ReservationHistory.java
+++ src/main/java/com/wooyano/wooyanomonolithic/webhook/domain/ReservationHistory.java
@@ -3,7 +3,9 @@ package com.wooyano.wooyanomonolithic.webhook.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,4 +17,20 @@ public class ReservationHistory {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
+
+    private String orderId;
+    private LocalDateTime createdAt;
+
+    @Builder
+    private ReservationHistory(String orderId, LocalDateTime createdAt) {
+        this.orderId = orderId;
+        this.createdAt = createdAt;
+    }
+
+    public static ReservationHistory create(String orderId, LocalDateTime createdAt) {
+        return ReservationHistory.builder()
+            .orderId(orderId)
+            .createdAt(createdAt)
+            .build();
+    }
 }
