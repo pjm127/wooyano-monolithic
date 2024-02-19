001434ad6b423081f665b2cc117adda9bf0e6bea
pjm127 <mikeywc40@gmail.com>
2024-02-19 15:59:31 +0900

    test: 멀티쓰레드 100명 동시 쿠폰 발급 테스트 코드 구현

--- src/test/java/com/wooyano/wooyanomonolithic/coupon/application/CouponServiceTest.java
+++ src/test/java/com/wooyano/wooyanomonolithic/coupon/application/CouponServiceTest.java
@@ -8,6 +8,10 @@ import com.wooyano.wooyanomonolithic.coupon.application.dto.CouponIssueServiceRe
 import com.wooyano.wooyanomonolithic.coupon.domain.Coupon;
 import com.wooyano.wooyanomonolithic.coupon.infrastructure.CouponRepository;
 import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,7 +43,7 @@ class CouponServiceTest {
         assertThat(findCoupon.getTotalQuantity()).isEqualTo(99);
     }
 
-    @DisplayName("쿠폰 이름이 존재하지 않으면 예외를 발생시킨다.")
+    @DisplayName("존재하지 않는 이름의 쿠폰 발급을 요청하면 예외를 발생시킨다.")
     @Test
     public void issueCouponWithNoName(){
         // given
@@ -52,6 +56,50 @@ class CouponServiceTest {
         assertThatThrownBy(() -> couponService.issueCoupon(request))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("존재하지 않는 쿠폰입니다.");
+    }
+
+//    @DisplayName("쿠폰 발급시 수량이 떨어지면 예외를 발생한다")
+//    @Test
+//    public void issueCouponWithNoQuantity(){
+//        // given
+//        Coupon coupon = createCoupon();
+//        couponRepository.save(coupon);
+//        CouponIssueServiceRequest request = CouponIssueServiceRequest.builder()
+//                .name("test1")
+//                .build();
+//        // when
+//        for (int i = 0; i < 101; i++) {
+//            couponService.issueCoupon(request);
+//        }
+//        // then
+//    }
+
+    @DisplayName("쿠폰 발급 동시에 100개를 요청한다")
+    @Test
+    public void issueCoupon_concurrency() throws InterruptedException {
+        // given
+        Coupon coupon = createCoupon();
+        couponRepository.save(coupon);
+        CouponIssueServiceRequest request = CouponIssueServiceRequest.builder()
+                .name("test1")
+                .build();
+
+        int threadSize = 100;
+        var executorService = Executors.newFixedThreadPool(threadSize);
+        var countDownLatch = new CountDownLatch(threadSize);
+
+        //when
+        for (int i = 0; i < threadSize; i++) {
+            executorService.submit(() -> {
+                couponService.issueCoupon(request);
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+
+        // then
+        Coupon findCoupon = couponRepository.findByName(request.getName()).get();
+        assertThat(findCoupon.getTotalQuantity()).isEqualTo(0);
     }
 
     private Coupon createCoupon(){
